a867b90c70d44ae2a6f64b473a8ebad255ee1a14
Jacob Hyde <jacobosity@gmail.com>
2020-04-07 04:45:13 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,29 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+ array.index(value_to_find)
+end
+
+def find_max_value(array)
+  counter = 0 
+  max_value = array[0]
+ 
+  while counter < array.length do
+    if array[counter] > max_value
+      max_value = array[counter]
+    end
+    counter += 1
+  end
+  max_value
+end
+
+def find_min_value(array)
+  counter = 0 
+  min_value = array[0]
+ 
+  while counter < array.length do
+    if array[counter] < min_value
+      min_value = array[counter]
+    end
+    counter += 1
+  end
+  min_value
 end
